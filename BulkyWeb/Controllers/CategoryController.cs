7695074be490c7c779562d09634eedd9b1ab3ac5
PalakYadav-0910 <sy8220718@gmail.com>
2025-07-27 10:09:02 +0530

    Retrieve category from Database for Edit Opeartion

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -42,5 +42,44 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Find works on Primary Key only (Here it is Id)
+            Category ? categoryFromDb = _db.Categories.Find(id);
+            //Category ? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category ? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //Adding Custom Validation
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                //Not Bind to any property, Key=empty string
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+            return View();
+        }
     }
 }
